ea445916ab275a82c7ab2c4b42be91afb7eeaa82
Prashanth Sp <prashanth.sp98@gmail.com>
2018-11-21 14:29:15 +0530

    added something

--- app.py
+++ app.py
@@ -12,11 +12,6 @@ app.secret_key = "thisisscreteithink"
 def page():
     return render_template('home.html')
 
-
-
-
-
-
 #admin route start here;
 
 @app.route('/admin',methods=['POST','GET'])
@@ -33,43 +28,34 @@ def admin():
 @app.route('/admin/home')
 def adminHome():
     if 'user' in session:
-        return render_template('admin_home.html',username=session['user'])
-
-
-
-
-class StudentRegistration(Form):
-   name = TextField("Name",[validators.Required("Please enteryour name.")])
-   gender = RadioField('Gender', choices = [('M','Male'),('F','Female')])
-   usn = TextField("usn",[validators.Required("Please enteryour usn.")])
-   sem = IntegerField("sem",[validators.Required("Please enteryour sem.")])
-   phone = IntegerField("phone",[validators.Required("Please enteryour phone.")])
-   email = TextField("Email",[validators.Required("Please enter your email address."),validators.Email("Please enter your email address.")])
-   department = SelectField('Department', choices = [('cse', 'cse'),('ise', 'ise'),('ise', 'ise'),('mech', 'mech'),('civil', 'civil')])
-   password = PasswordField("password",[validators.Required()])
-   submit = SubmitField("Submit")
-
+        c,conn = connection()
+        c.execute("SELECT COUNT(id) FROM student")
+        student_data = c.fetchone()
+        c.execute("SELECT COUNT(id) FROM teacher")
+        teacher_data = c.fetchone()
+        return render_template('admin_home.html',username=session['user'],student_data=student_data,teacher_data=teacher_data)
 
 @app.route('/admin/studentRegister',methods=['GET', 'POST'])
 def studentRegister():
     if 'user' in session:
-        form = StudentRegistration(request.form)
-        if request.method=="POST" and form.validate():
-            name = form.name.data
-            email = form.email.data
-            usn = form.usn.data
-            phone = form.phone.data
-            sem = form.phone.data
-            gender = form.gender.data
-            department = form.department.data
-            password = form.password.data
+        if request.method=="POST":
+            name=request.form['name']
+            email=request.form['email']
+            usn=request.form['usn']
+            phone=request.form['phone']
+            sem=request.form['sem']
+            gender=request.form['gender']
+            department=request.form['department']
+            password=request.form['password']
             c,con = connection()
             #x=c.execute("SELECT * FROM student WHERE usn=%s"% con.literal(usn))
-            c.execute("INSERT INTO student (usn,name,sem,phone,department,email,password,gender) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",(usn,name,sem,phone,department,email,password,gender))
+            c.execute("INSERT INTO student (usn,name,sem,phone,department,email,password,gender) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",[usn,name,sem,phone,department,email,password,gender])
+            con.commit()
+            c.execute("INSERT INTO attendance(usn) values(%s)",[usn])
             con.commit()
             return  redirect(url_for('adminHome'))
         else:
-            return render_template('student_register.html',form=form)
+            return render_template('student_register.html')
     else:
         return redirect(url_for(admin))
 
@@ -77,40 +63,64 @@ def studentRegister():
 @app.route('/admin/home/logout')
 def adminLogout():
     session.pop('user',None)
-    return redirect(url_for('admin'))
-
-
-
-class TeacherRegistration(Form):
-   teacher_name = TextField("Name",[validators.Required("Please enter your name.")])
-   gender = RadioField('Gender', choices = [('M','Male'),('F','Female')])
-   empid = TextField("empid",[validators.Required("Please enteryour empid.")])
-   phone = IntegerField("phone",[validators.Required("Please enteryour phone.")])
-   email = TextField("Email",[validators.Required("Please enter your email address."),validators.Email("Please enter your email address.")])
-   department = SelectField('Department', choices = [('cse', 'cse'),('ise', 'ise'),('ece', 'ece'),('mech', 'mech'),('civil', 'civil')])
-   password = PasswordField("password",[validators.Required()])
-   submit = SubmitField("Submit")
-
+    return redirect(url_for('page'))
 
 
 @app.route('/admin/teacherRegister',methods=['GET', 'POST'])
 def teacherRegister():
     if 'user' in session:
-        form = TeacherRegistration(request.form)
-        if request.method=="POST" and form.validate():
-            teacher_name = form.teacher_name.data
-            email = form.email.data
-            empid = form.empid.data
-            phone = form.phone.data
-            gender = form.gender.data
-            department = form.department.data
-            password = form.password.data
+        if request.method=="POST":
+            teacher_name = request.form['name']
+            email = request.form['email']
+            empid = request.form['empid']
+            phone = request.form['phone']
+            gender = request.form['gender']
+            department = request.form['department']
+            salary = request.form['salary']
+            password = request.form['password']
             c,con = connection()
-            c.execute("INSERT INTO teacher (empid,name,phone,department,email,password,gender) VALUES(%s,%s,%s,%s,%s,%s,%s)",(empid,teacher_name,phone,department,email,password,gender))
+            c.execute("INSERT INTO teacher (empid,name,phone,department,email,password,gender,salary) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",(empid,teacher_name,phone,department,email,password,gender,salary))
             con.commit()
             return  redirect(url_for('adminHome'))
         else:
-            return render_template('teacher_registration.html',form=form)
+            return render_template('teacher_registration.html')
+    else:
+        return redirect(url_for(admin))
+
+
+@app.route('/admin/studentInfo',methods=['GET', 'POST'])
+def studentInfo():
+    if 'user' in session:
+        if request.method=="POST":
+            usn=request.form['usn']
+            c,con = connection()
+            c.execute("SELECT * FROM student WHERE usn=%s",[usn])
+            data = c.fetchone()
+            if data:
+                return render_template('student_info.html',data=data)
+            else:
+                error="no record found"
+                return render_template('student_info.html',error=error)
+        else:
+            return render_template('student_info.html')
+    else:
+        return redirect(url_for(admin))
+
+@app.route('/admin/teacherInfo',methods=['GET', 'POST'])
+def teacherInfo():
+    if 'user' in session:
+        if request.method=="POST":
+            empid = request.form['empid']
+            c,con = connection()
+            c.execute("SELECT * FROM teacher WHERE empid=%s",[empid])
+            data = c.fetchone()
+            if data:
+                return render_template('teacher_info.html',data=data)
+            else:
+                error="no record found"
+                return render_template('teacher_info.html',error=error)
+        else:
+            return render_template('teacher_info.html')
     else:
         return redirect(url_for(admin))
 
@@ -122,10 +132,10 @@ def login():
     if request.method =='POST':
         c, conn = connection()
         c.execute("SELECT * FROM student WHERE usn=%s",[request.form['usn']])
-        data = c.fetchone()[6]
+        data = c.fetchone()[7]
         if data == request.form['password']:
             c.execute("SELECT * FROM student WHERE usn=%s",[request.form['usn']])
-            d = c.fetchone()[0]
+            d = c.fetchone()[1]
             session['user']= d
             return redirect(url_for('student_profile'))
         else:
@@ -166,7 +176,7 @@ def viewattendance():
 @app.route('/student/student_profile/logout')
 def studentLogout():
     session.pop('user',None)
-    return redirect(url_for('login'))
+    return redirect(url_for('page'))
 
 #teachers route start here;
 
@@ -176,10 +186,10 @@ def teacher_login():
     if request.method =='POST':
         c, conn = connection()
         c.execute("SELECT * FROM teacher WHERE empid=%s",[request.form['empid']])
-        data = c.fetchone()[5]
+        data = c.fetchone()[6]
         if data == request.form['password']:
             c.execute("SELECT * FROM teacher WHERE empid=%s",[request.form['empid']])
-            d = c.fetchone()[0]
+            d = c.fetchone()[1]
             session['user']= d
             return redirect(url_for('teacher_profile'))
         else:
@@ -198,70 +208,68 @@ def teacher_profile():
     else:
         return redirect(url_for('login'))
 
-
-class UpdateMarks(Form):
-   usn = TextField("usn",[validators.Required("Please enter usn.")])
-   subject_name = RadioField('subjects', choices = [('finite automata','finite automata'),
-                                                    ('computer networks','computer networks'),
-                                                    ('oo modeling and design','oo modeling and design'),
-                                                    ('dbms','dbms'),
-                                                    ('management','management'),
-                                                    ('DotNet','DotNet'),
-                                                    ('computer networks lab','computer networks lab'),
-                                                    ('dbms lab','dbms lab')])
-   internal1 = IntegerField("internal1",[validators.Required(".")])
-   internal2 = IntegerField("internal2",[validators.Required("")])
-   internal3 = IntegerField("internal3",[validators.Required("")])
-   submit = SubmitField("Submit")
-
 @app.route('/teacher/update_marks',methods=['GET', 'POST'])
 def updatemarks():
-    form = UpdateMarks(request.form)
-    if request.method=="POST" and form.validate():
-        usn = form.usn.data
-        subject_name = form.subject_name.data
-        internal1 = form.internal1.data
-        internal2 = form.internal2.data
-        internal3 = form.internal3.data
-        c,con = connection()
-        c.execute("INSERT INTO marks (usn,subject_name,internal1,internal2,internal3) VALUES(%s,%s,%s,%s,%s)",(usn,subject_name,internal1,internal2,internal3))
-        con.commit()
-        return  redirect(url_for('teacher_profile'))
-    else:
-        return render_template('update_marks.html',form=form)
+    if 'user' in session:
+        if request.method=="POST":
+            usn = request.form['usn']
+            subject_name = request.form['subject_name']
+            sem = request.form['sem']
+            internal1 = request.form['internal1']
+            internal2 = request.form['internal2']
+            internal3 = request.form['internal3']
+            c,con = connection()
+            c.execute("INSERT INTO marks (usn,subject_name,internal1,internal2,internal3,sem) VALUES(%s,%s,%s,%s,%s,%s)",[usn,subject_name,internal1,internal2,internal3,sem])
+            con.commit()
+            return  redirect(url_for('teacher_profile'))
+        else:
+            return render_template('update_marks.html')
 
-class UpdateAttendance(Form):
-   usn = TextField("usn",[validators.Required("Please enter usn.")])
-   subject_name = RadioField('subjects', choices = [('finite automata','finite automata'),
-                                                    ('computer networks','computer networks'),
-                                                    ('oo modeling and design','oo modeling and design'),
-                                                    ('dbms','dbms'),
-                                                    ('management','management'),
-                                                    ('DotNet','DotNet'),
-                                                    ('computer networks lab','computer networks lab'),
-                                                    ('dbms lab','dbms lab')])
-   total_present = IntegerField("total present",[validators.Required(".")])
-   total_class = IntegerField("total class",[validators.Required("")])
-   submit = SubmitField("Submit")
+    else:
+        return redirect(url_for('teacher_login'))
 
 @app.route('/teacher/update_attendance',methods=['GET', 'POST'])
 def updateattendance():
-    form = UpdateAttendance(request.form)
-    if request.method=="POST" and form.validate():
-        usn = form.usn.data
-        subject_name = form.subject_name.data
-        total_present = form.total_present.data
-        total_class = form.total_class.data
-        c,con = connection()
-        c.execute("UPDATE attendance set total_present=%s,total_class=%s WHERE usn=%s AND subject_name=%s",(total_present,total_class,usn,subject_name))
-        con.commit()
-        return  redirect(url_for('teacher_profile'))
+    if 'user' in session:
+        if request.method=="POST":
+            usn = request.form['usn']
+            subject_name = request.form['subject_name']
+            total_present = request.form['total_present']
+            total_class = request.form['total_class']
+            c,con = connection()
+            c.execute("UPDATE attendance SET total_present=%s,total_class=%s,subject_name=%s WHERE usn=%s ",(total_present,total_class,subject_name,usn))
+            con.commit()
+            return  redirect(url_for('teacher_profile'))
+        else:
+            return render_template('update_attendance.html')
+
     else:
-        return render_template('update_attendance.html',form=form)
+        return redirect(url_for('teacher_login'))
 
 
+@app.route('/teacher/student_Info',methods=['GET', 'POST'])
+def StudentInfo():
+    if 'user' in session:
+        if request.method=="POST":
+            usn=request.form['usn']
+            c,con = connection()
+            c.execute("SELECT * FROM student WHERE usn=%s",[usn])
+            data = c.fetchone()
+            c.execute("SELECT * FROM marks where usn=%s",[usn])
+            marks = c.fetchall()
+            c.execute("SELECT * FROM attendance WHERE usn=%s",[usn])
+            attendance = c.fetchall()
+            if data:
+                return render_template('teacher_studentinfo.html',data=data,marks=marks,attendance=attendance)
+            else:
+                error="no record found"
+                return render_template('teacher_studentinfo.html',error=error)
+        else:
+            return render_template('teacher_studentinfo.html')
+    else:
+        return redirect(url_for(admin))
 
 @app.route('/teacher/home/logout')
 def teacherLogout():
     session.pop('user',None)
-    return redirect(url_for('teacher_login'))
+    return redirect(url_for('page'))

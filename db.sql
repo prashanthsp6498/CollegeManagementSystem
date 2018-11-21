962c35f6eaeba6814b0cf78fa2a11ffb762c4f28
Prashanth Sp <prashanth.sp98@gmail.com>
2018-11-21 14:31:17 +0530

    modified database

--- db.sql
+++ db.sql
@@ -23,14 +23,12 @@ DROP TABLE IF EXISTS `attendance`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `attendance` (
-  `usn` varchar(50) DEFAULT NULL,
-  `subject_name` varchar(30) DEFAULT NULL,
+  `usn` varchar(40) DEFAULT NULL,
+  `subject_name` varchar(80) DEFAULT NULL,
   `total_present` int(11) DEFAULT NULL,
   `total_class` int(11) DEFAULT NULL,
-  KEY `usn` (`usn`),
   KEY `subject_name` (`subject_name`),
-  CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`usn`) REFERENCES `student` (`usn`) ON DELETE CASCADE,
-  CONSTRAINT `attendance_ibfk_2` FOREIGN KEY (`subject_name`) REFERENCES `subjects` (`subject_name`) ON DELETE CASCADE
+  CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`subject_name`) REFERENCES `subjects` (`subject_name`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -40,7 +38,7 @@ CREATE TABLE `attendance` (
 
 LOCK TABLES `attendance` WRITE;
 /*!40000 ALTER TABLE `attendance` DISABLE KEYS */;
-INSERT INTO `attendance` VALUES ('4mn16cs001','computer networks',1,2),('4mn16cs001','dbms',1,2),('4mn16cs001','management',1,2),('4mn16cs001','oo modeling and design',1,2),('4mn16cs001','DotNet',1,2),('4mn16cs001','finite automata',1,2),('4mn16cs001','computer networks lab',1,2),('4mn16cs001','dbms lab',1,2),('4mn16cs002','computer networks',NULL,NULL),('4mn16cs002','dbms',NULL,NULL),('4mn16cs002','management',NULL,NULL),('4mn16cs002','oo modeling and design',NULL,NULL),('4mn16cs002','DotNet',NULL,NULL),('4mn16cs002','finite automata',NULL,NULL),('4mn16cs002','computer networks lab',NULL,NULL),('4mn16cs002','dbms lab',NULL,NULL),('4mn16cs003','computer networks',NULL,NULL),('4mn16cs003','dbms',NULL,NULL),('4mn16cs003','management',NULL,NULL),('4mn16cs003','oo modeling and design',NULL,NULL),('4mn16cs003','DotNet',NULL,NULL),('4mn16cs003','finite automata',NULL,NULL),('4mn16cs003','computer networks lab',NULL,NULL),('4mn16cs003','dbms lab',NULL,NULL),('4mn16cs004','computer networks',NULL,NULL),('4mn16cs004','dbms',NULL,NULL),('4mn16cs004','management',NULL,NULL),('4mn16cs004','oo modeling and design',NULL,NULL),('4mn16cs004','DotNet',NULL,NULL),('4mn16cs004','finite automata',NULL,NULL),('4mn16cs004','computer networks lab',NULL,NULL),('4mn16cs004','dbms lab',NULL,NULL),('4mn16cs031','computer networks',20,20),('4mn16cs031','dbms',25,26),('4mn16cs031','management',20,21),('4mn16cs031','oo modeling and design',19,20),('4mn16cs031','DotNet',25,25),('4mn16cs031','finite automata',20,21),('4mn16cs031','computer networks lab',19,21),('4mn16cs031','dbms lab',15,15);
+INSERT INTO `attendance` VALUES ('4mn16cs031','DotNet',19,20),('4mn16cs032','DotNet',15,20),('4mn17is001','DotNet',20,20);
 /*!40000 ALTER TABLE `attendance` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,11 +73,12 @@ DROP TABLE IF EXISTS `marks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `marks` (
-  `usn` varchar(30) DEFAULT NULL,
-  `subject_name` varchar(30) DEFAULT NULL,
-  `internal1` int(10) DEFAULT NULL,
-  `internal2` int(10) DEFAULT NULL,
-  `internal3` int(10) DEFAULT NULL,
+  `usn` varchar(90) DEFAULT NULL,
+  `subject_name` varchar(90) DEFAULT NULL,
+  `internal1` int(11) DEFAULT NULL,
+  `internal2` int(11) DEFAULT NULL,
+  `internal3` int(11) DEFAULT NULL,
+  `sem` int(11) DEFAULT NULL,
   KEY `usn` (`usn`),
   KEY `subject_name` (`subject_name`),
   CONSTRAINT `marks_ibfk_1` FOREIGN KEY (`usn`) REFERENCES `student` (`usn`) ON DELETE CASCADE,
@@ -93,7 +92,7 @@ CREATE TABLE `marks` (
 
 LOCK TABLES `marks` WRITE;
 /*!40000 ALTER TABLE `marks` DISABLE KEYS */;
-INSERT INTO `marks` VALUES ('4mn16cs031','finite automata',20,20,20),('4mn16cs002','finite automata',19,18,17),('4mn16cs031','computer networks',20,20,20);
+INSERT INTO `marks` VALUES ('4mn16cs031','DotNet',15,15,15,1),('4mn16cs031','dbms',20,20,20,1),('4mn17is001','DotNet',10,10,10,6);
 /*!40000 ALTER TABLE `marks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -105,18 +104,20 @@ DROP TABLE IF EXISTS `student`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `student` (
-  `usn` varchar(30) NOT NULL,
-  `name` varchar(30) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usn` varchar(50) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
   `sem` int(11) DEFAULT NULL,
-  `phone` int(11) DEFAULT NULL,
-  `department` varchar(30) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL,
-  `password` varchar(30) DEFAULT NULL,
-  `gender` varchar(20) DEFAULT NULL,
+  `phone` int(15) DEFAULT NULL,
+  `department` varchar(50) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`usn`),
+  UNIQUE KEY `id` (`id`),
   KEY `department` (`department`),
-  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`department`) REFERENCES `department` (`dep_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`department`) REFERENCES `department` (`dep_name`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +126,7 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES ('444asdf','dafjf',8845,8845,'cse','asdf@gmail.com','aawseddrf','M'),('4mn16cs001','Akshay',5,99558844,'cse','akshay@gmail.com','akshay','m'),('4mn16cs002','Abhi',5,98548844,'cse','abhi@gmail.com','abhi','m'),('4mn16cs003','Abhilash',5,78148844,'ise','abhilash@gmail.com','abhilash','m'),('4mn16cs004','Akhila',5,78147844,'ece','akhila@gmail.com','akhila','f'),('4mn16cs031','Prashanth S P',884455,884455,'cse','prashanth.sp98@gmail.com','asswa','M');
+INSERT INTO `student` VALUES (2,'4mn16cs001','Bill gate',5,81239,'cse','bill@gmail.com','bill','male'),(3,'4mn16cs003','akash',5,8844,'cse','akash@gmail.com','akash','male'),(1,'4mn16cs031','Prashanth S P',1,45,'cse','prashanth.sp98@gmail.com','asdf','male'),(5,'4mn16cs032','Punith',5,554455,'cse','punith@gmail.com','4mn16cs032','male'),(4,'4mn16cs040','Sudharshan',5,56622,'cse','sudharshan@gmail.com','4mn16cs040','male'),(6,'4mn17is001','srivathsa',6,4545,'ise','ads@gmail.com','asdf','male');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -164,17 +165,20 @@ DROP TABLE IF EXISTS `teacher`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `teacher` (
-  `empid` varchar(30) NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
-  `phone` int(30) DEFAULT NULL,
-  `department` varchar(30) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL,
-  `password` varchar(60) DEFAULT NULL,
-  `gender` varchar(20) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `empid` varchar(80) NOT NULL,
+  `name` varchar(90) DEFAULT NULL,
+  `phone` int(15) DEFAULT NULL,
+  `department` varchar(90) DEFAULT NULL,
+  `email` varchar(90) DEFAULT NULL,
+  `password` varchar(90) DEFAULT NULL,
+  `gender` varchar(90) DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
   PRIMARY KEY (`empid`),
+  UNIQUE KEY `id` (`id`),
   KEY `department` (`department`),
   CONSTRAINT `teacher_ibfk_1` FOREIGN KEY (`department`) REFERENCES `department` (`dep_name`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -183,7 +187,7 @@ CREATE TABLE `teacher` (
 
 LOCK TABLES `teacher` WRITE;
 /*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
-INSERT INTO `teacher` VALUES ('asdf456','elon musk',47558,'cse','elon@gmail.com','asdfghjk','M');
+INSERT INTO `teacher` VALUES (1,'adf','adf',55,'cse','prashanth.sp98@gmail.com','asdf','male',5000);
 /*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -196,4 +200,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-13 16:05:19
+-- Dump completed on 2018-11-21 14:30:40
